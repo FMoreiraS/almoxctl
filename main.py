45583c064fc9e6f191113eb5bd1f0b9a28c9c678
FMoreiraS <mfellipeds@hotmail.com>
2026-06-05 11:04:33 -0300

    feat: add Main class inside main.py

--- main.py
+++ main.py
@@ -15,5 +15,47 @@ class Movement:
         self.type = type
         self.responsible = responsible
 
+class Main:
+    @staticmethod
+    def main():
+        print('BEM-VINDO AO ALMOXCTL')
+    
+        while True:
+            print('1. Adicionar produto no estoque')
+            print('2. Retirar produto do estoque')
+            print('3. Exibir produtos em estoque')
+            print('4. Exibir movimentações no estoque')
+            print('5. Sair')
+            
+            option = 0
+            try:
+                option = int(input())
+            except ValueError:
+                print('Valor inadequado. Tente um número de 1 a 5')
+
+
+            match option:
+                case 1:
+                    Main.addProduct()
+                case 2:
+                    Main.removeProduct()
+                case 3:
+                    pass
+                case 4:
+                    pass
+                case 5:
+                    print('Programa encerrado')
+                    break
+                case _:
+                    print('Opção inválida')
+
+    @staticmethod
+    def addProduct():
+        print('Produto adicionado com sucesso')
+
+    @staticmethod
+    def removeProduct():
+        print('Produto removido com sucesso')
+
 if __name__ == '__main__':
-    print('BEM-VINDO AO ALMOXCTL')
+    Main.main()
